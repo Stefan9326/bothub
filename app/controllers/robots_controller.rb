e2f0cf78fee6822c6add3581ab79b75d6274014a
Pouyan Soltanpour <soltanpour.p@gmail.com>
2023-05-29 22:51:24 +0100

    controllers and routes done

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,22 @@
+class RobotsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def availability
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
+
+  resources :robots do
+    member do
+      get :availability
+    end
+  end
+
+  resources :bookings, only: [:index, :create]
+
+  resources :users, only: [] do
+    resources :rentals, only: [:index]
+  end
 end

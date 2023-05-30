180ebbf24ddd1949e3c38cb6f3c02cdf8eda8d4a
Stefan Shambrook <stefshambrook@gmail.com>
2023-05-30 12:48:38 +0100

    fix missing migrations

--- db/migrate/20230530105711_remove_availability_from_robots.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvailabilityFromRobots < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :robots, :availability, :boolean
-  end
-end
---
+++ db/migrate/20230530114712_create_robots.rb
@@ -0,0 +1,13 @@
+class CreateRobots < ActiveRecord::Migration[7.0]
+  def change
+    create_table :robots do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.boolean :availability
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

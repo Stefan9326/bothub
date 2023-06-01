f676f1828e526fb7fa7dc96ff8cebc6e4a38945d
Stefan Shambrook <stefshambrook@gmail.com>
2023-06-01 15:06:36 +0100

    fix stimulus import and improve search

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,5 +10,4 @@ eagerLoadControllersFrom("controllers", application)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
 // app/javascript/controllers/index.js
-import { Application } from "stimulus"
-import DeleteController from "./delete_controller"
+import { Application } from "@hotwired/stimulus"
